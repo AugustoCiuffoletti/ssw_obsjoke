0cb77eb42858937235fab4eacb87b09d50e0ccce
AugustoCiuffoletti <augusto@di.unipi.it>
2020-09-23 09:53:32 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 // Produce una battuta su Chuck Norris
-import { Observable } from 'rxjs';
-import { ajax } from 'rxjs/ajax';
+import { Observable } from "rxjs";
+import { ajax } from "rxjs/ajax";
 
-ajax("https://api.chucknorris.io/jokes/random").
-	subscribe( 
-		data => console.log(data.response.value)
-	);
+ajax("https://api.chucknorris.io/jokes/random").subscribe(data =>
+  console.log(data.response.value)
+);
